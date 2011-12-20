9f05cdab69d8be32c8250faf498789a2e15d820c
Noah Goodrich <noah.goodrich@lendio.com>
2011-12-20 13:12:28 -0700

    Beginning work on the Oauth2_Server component

---
+++ classes/kohana/oauth2/server.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Description of server
+ *
+ * @author noah
+ */
+class Kohana_Oauth2_Server
+{
+	public static function factory()
+	{
+		return new Oauth2_Server();
+	}
+
+	protected function __construct()
+	{
+		
+	}
+}
---
+++ classes/oauth2/server.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Description of server
+ *
+ * @author Noah Goodrich
+ */
+class Oauth2_Server extends Kohana_Oauth2_Server
+{
+
+}
