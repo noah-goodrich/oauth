f8b8236af3f53ccc6e070235ae73a9b316ddf775
Noah Goodrich <goodrich.noah@gmail.com>
2012-12-16 20:46:52 -0700

    Converted classes for 3.3/master

---
+++ classes/Kohana/Oauth2/Provider/Gmail.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @user: noah
+ * @date 12/16/12
+ */
+abstract class Kohana_Oauth2_Provider_Gmail extends OAuth2_Provider
+{
+	public $name = 'gmail';
+
+	public function url_authorize()
+	{
+		return 'https://accounts.google.com/o/oauth2/auth';
+	}
+
+	public function url_access_token()
+	{
+		return 'https://www.googleapis.com/oauth2/v1/tokeninfo';
+	}
+}
---
+++ classes/Oauth2/Provider/Gmail.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * @user: noah
+ * @date 12/16/12
+ */
+class Oauth2_Provider_Gmail extends Kohana_Oauth2_Provider_Gmail
+{
+
+}
